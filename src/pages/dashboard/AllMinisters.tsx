56cf5507890eb4014a92c71c67bb7521c047869a
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2025-02-06 19:48:41 +0100

    image

--- src/pages/dashboard/AllMinisters.tsx
+++ src/pages/dashboard/AllMinisters.tsx
@@ -18,6 +18,7 @@ const AllMinisters = () => {
         setMinisters(response.data.data as iMinisters[]);
 
         console.log("minsters-data", response.data.data)
+        // console.log(${url}/${response.data.data.ayoAweMinImage.replace(/\/g, "/")})
       } catch (err) {
         console.error("Error getting ministers data", err);
       } finally {
@@ -32,10 +33,13 @@ const AllMinisters = () => {
 
 
 
+
   return (
     <div className="md:w-[100%] min-h-screen bg-gray-40 p-6 mt-[100px] ml-[40px] flex items-cente justify-center bg-white">
         <div className="w-full shadow-m h-full flex flex-col gap-7 items-center p-8">
+          <p className='text-[16px] font-bold underline'>All Registered Ministers</p>
             <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {/* { loading ? "Loading..." : */}
                 {ministers.map((minister) => (
                 <motion.div
                     key={minister._id}
@@ -47,7 +51,9 @@ const AllMinisters = () => {
 
                     {minister.ayoAweMinImage ? (
                     <img
-                        src={`${url}/uploads/${minister.ayoAweMinImage.split('/').pop()}`}
+                        // src={`${url}/Uploads/images/${minister.ayoAweMinImage}`}
+                        src={minister?.ayoAweMinImage}
+                        // src={`${url}/${minister.ayoAweMinImage}`}
                         alt={minister.firstName}
                         className="w-32 h-32 rounded-full object-cover border-4 border-gray-200"
                     />
@@ -61,27 +67,28 @@ const AllMinisters = () => {
                     alt={minister.firstName}
                     className="w-32 h-32 rounded-full object-cover border-4 border-gray-200"
                     /> */}
-                    <h2 className="text-xl font-bold mt-3">{minister.title} {minister.firstName} {minister.middleName ? minister.middleName : ''} {minister.lastName}</h2>
+                    <h2 className="text-xl font-bold mt-3 text-center">{minister.title} {minister.firstName} {minister.middleName ? minister.middleName : ''} {minister.lastName}</h2>
 
                     <p className="text-[#000] font-bold">{minister.gender}</p>
                     
                     <p className="text-[#000] font-bold text-center">{minister.city}, {minister.state}, {minister.country}</p>
                     
-                    <p className="text-sm text-[#000]">Phone Number: <span className='text-[#000] font-bold'>{minister.phoneNumber}</span></p>
+                    <p className="text-sm text-[#000]">Phone Number: <span className='text-[#000] font-bold text-center'>{minister.phoneNumber}</span></p>
 
-                    <p className="text-sm text-[#000]">WhatsApp Number: <span className='text-[#000] font-bold'>{minister.whatsapp}</span></p>
+                    <p className="text-sm text-[#000] text-center">WhatsApp Number: <span className='text-[#000] font-bold'>{minister.whatsapp}</span></p>
 
-                    <p className="text-sm text-blue-500  cursor-pointer mt-1"><span className='text-[#000]'>Email:</span> {minister.email}</p>
+                    <p className="text-sm text-blue-500  cursor-pointer mt-1 text-center"><span className='text-[#000]'>Email:</span> {minister.email}</p>
 
-                    <p className="text-sm text-[#000] mt-2">Current Ministry: <span className='text-[#000] font-bold'>{minister.whichMinistry}</span></p>
+                    <p className="text-sm text-[#000] mt-2 text-center">Current Ministry: <span className='text-[#000] font-bold'>{minister.whichMinistry}</span></p>
 
-                    <p className="text-sm text-[#000]">Called to Ministry?: <span className='text-[#000] font-bold'>{minister.ministryCall}</span></p>
+                    <p className="text-sm text-[#000] text-center">Called to Ministry?: <span className='text-[#000] font-bold'>{minister.ministryCall}</span></p>
 
-                    {minister.other && <p className="text-sm text-[#000] italic mt-2">Note: <span className='text-[#000] font-bold'>{minister.other}</span></p>}
+                    {minister.other && <p className="text-sm text-[#000] italic mt-2 text-center">Note: <span className='text-[#000] font-bold'>{minister.other}</span></p>}
 
                     <p className="text-sm text-[#000] mt-2 text-center">Why do you want to join the training?: <span className='text-[#000] font-bold'>{minister.why}</span></p>
                 </motion.div>
                 ))}
+                {/* } */}
             </div>
         </div>
     </div>
